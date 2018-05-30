29485b1a2a25f20d2bc7b412f74b82cfc9057f2d
Carter Robinson <cartertrobinson@gmail.com>
2018-05-30 15:59:08 -0500

    Began work on server and basic Yelp API request

--- server/index.js
+++ server/index.js
@@ -1,32 +1,49 @@
 require('dotenv').config();
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const request = require('request');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
 
-var app = express();
+const app = express();
+
+app.use(bodyParser.json());
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(`${__dirname}/../react-client/dist`));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
+app.post('/search', (req, res) => {
+  const { zip } = req.body;
+  let options = {
+    method: 'GET',
+    uri: 'https://api.yelp.com/v3/businesses/search',
+    headers: {
+      Authorization: process.env.YELP_API_KEY,
+    },
+    qs: {
+      location: zip,
     }
+  };
+  request(options, (err, data) => {
+    if (err) {
+      console.log('Error in interacting with the Yelp API', err);
+      res.statusCode(404).end();
+    }
+    // To Do: Add code to pass all this data on to the database
+    res.send(data.body);
   });
+
+  
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port', process.env.PORT || 3000);
 });
 
