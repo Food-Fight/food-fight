ae77b11ebb1b9f301c647280d4059a13c863da6c
Carter Robinson <cartertrobinson@gmail.com>
2018-06-02 12:23:57 -0500

    Added CreateRoom component that generates a unique URL and automatically emails users

---
+++ react-client/src/components/CreateRoom.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import $ from "jquery";
+import uniqueString from "unique-string";
+
+class CreateRoom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      emails: [],
+      roomID: null
+    };
+  }
+
+  addEmail() {
+    this.state.emails.push(this.state.query);
+    this.setState({
+      query: ""
+    });
+  }
+  
+  createRoom() {
+    this.state.emails.forEach((email) => this.sendInvite(email));
+    //TO DO: SEND THIS ROOM INFORMATION TO THE DATABASE
+  }
+  
+  createUniqueID() {
+    this.setState({
+      roomID: uniqueString()
+    });
+  }
+  
+  sendInvite(email) {
+    $.post("/api/email", { email: email, id: this.state.roomID}, (data, status) => {
+      console.log(`Email sent to ${email}:`, status);
+    });
+  }
+  
+  updateQuery(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
+
+  render() {
+    var uniqueURL = this.state.roomID ? 
+      `https://food-fight-greenfield.herokuapp.com/${this.state.roomID}`
+      : "";
+    return (
+      <div>
+        <h1>Create a Room</h1>
+        <div>
+          <button onClick={this.createUniqueID.bind(this)}>
+            Create a Unique URL{" "}
+          </button>
+          {uniqueURL}
+        </div>
+        Enter an email:
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={this.updateQuery.bind(this)}
+        />{" "}
+        <button onClick={this.addEmail.bind(this)}> Invite </button>
+        {this.state.emails.map((email, index) => {
+          return <li key={index}>Invitation will be sent to {email}</li>;
+        })}
+        <div>
+          <button onClick={this.createRoom.bind(this)}>Create Room</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateRoom;
